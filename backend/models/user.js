2e3d0b3224ed15d2d7dc0d43e1c86db0ed1aeb3a
Kiet Nguyen <kanguyen.vn@gmail.com>
2020-08-08 03:03:18 -0500

    Fixed bugs, now able to POST to /api/users

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const maxYear = new Date().getFullYear + 5;
+//var maxYear = new Date().getFullYear + 5;
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -28,8 +28,7 @@ const userSchema = new mongoose.Schema({
     class: {
         type: Number,
         required: true,
-        min: 1900,
-        max: maxYear
+        min: 1900
     },
     major: {
         type: String,
@@ -48,16 +47,16 @@ userSchema.methods.generateAuthToken = function () {
 const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
-    const schema = {
+    const schema = Joi.object({
         name: Joi.string().min(5).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(255).required(),
-        class: Joi.number().min(1900).max(maxYear).required(),
+        class: Joi.number().min(1900).required(),
         major: Joi.string().min(5).max(255).required(),
-    };
+    });
 
-    return Joi.validate(user, schema);
+    return schema.validate(user);
 }
 
 exports.User = User;
-exports.validate = validateUser;
+exports.validateUser = validateUser;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,15 +4,16 @@ const _ = require('lodash');
 const validate = require('../middleware/validate');
 const { User } = require('../models/user');
 const express = require('express');
+const winston = require('winston/lib/winston/config');
 const router = express.Router();
 
 function validateUser(req) {
-    const schema = {
+    const schema = Joi.object({
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(255).required()
-    };
+    });
 
-    return Joi.validate(req, schema);
+    return schema.validate(req);
 }
 
 router.post('/', validate(validateUser), async (req, res) => {
