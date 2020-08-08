3235248a4fd18d76f2f2f8c7a35c55bdb81ca224
Kiet Nguyen <kanguyen.vn@gmail.com>
2020-08-08 03:46:57 -0500

    Added routes for courses, departments, fixed some bugs, database now works preliminarily

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const departments = require('../routes/departments');
+const courses = require('../routes/courses');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
 const error = require('../middleware/error');
 
 module.exports = function (app) {
     app.use(express.json());
+    app.use('/api/departments', departments);
+    app.use('/api/courses', courses);
     app.use('/api/users', users);
     app.use('/api/auth', auth);
     app.use(error);
