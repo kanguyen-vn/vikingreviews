6d51447d47290ee13ba76389997ef505c2554293
Kiet Nguyen <kanguyen.vn@gmail.com>
2020-08-08 22:59:32 -0500

    Merged

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,40 +1,18 @@
 const auth = require('../middleware/auth');
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// const jwt = require('jsonwebtoken');
-// const config = require('config');
-
->>>>>>> 4579e3c266e1e4d962386b99d1f6250d363104bf
-=======
-const jwt = require('jsonwebtoken');
-const config = require('config');
->>>>>>> parent of 42b6f72... Added routes for courses, departments, fixed some bugs, database now works preliminarily
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { User, validateUser } = require('../models/user');
 const validate = require('../middleware/validate');
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-
-router.get('/me', auth, async (req, res) => { // localhost:3000/api/users/me
-=======
 // const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
 router.get('/me', auth, async (req, res) => {
->>>>>>> 4579e3c266e1e4d962386b99d1f6250d363104bf
     const user = await User.findById(req.user._id).select('-password');
     res.send(user);
 });
 
-<<<<<<< HEAD
-router.post('/', validate(validateUser), async (req, res) => { // localhost:3000/api/users
-=======
 router.post('/', validate(validateUser), async (req, res) => {
->>>>>>> 4579e3c266e1e4d962386b99d1f6250d363104bf
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User is already registered.');
 
