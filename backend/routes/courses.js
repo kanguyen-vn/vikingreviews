da8553bceae900942ece53c71f23b8120dfb45b6
Kiet Nguyen <kanguyen.vn@gmail.com>
2020-08-09 13:41:49 -0500

    Added validateObjectId to request handlers

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -2,6 +2,7 @@ const { Course, validateCourse } = require('../models/course');
 const validate = require('../middleware/validate');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const validateObjectId = require('../middleware/validateObjectId');
 const { Department } = require('../models/department');
 const express = require('express');
 const router = express.Router();
@@ -26,7 +27,7 @@ router.post('/', [auth, admin, validate(validateCourse)], async (req, res) => {
     res.send(course);
 });
 
-router.put('/:id', validate(validateCourse), async (req, res) => {
+router.put('/:id', [validateObjectId, validate(validateCourse)], async (req, res) => {
     const department = await Department.findById(req.body.department);
     if (!department) return res.status(400).send('Invalid department.');
 
@@ -43,7 +44,7 @@ router.put('/:id', validate(validateCourse), async (req, res) => {
     res.send(course);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
     const course = await Course.findByIdAndRemove(req.params.id);
 
     if (!course) return res.status(404).send('The course with the given ID was not found.');
@@ -51,7 +52,7 @@ router.delete('/:id', async (req, res) => {
     res.send(course);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validateObjectId, async (req, res) => {
     const course = await Course.findById(req.params.id);
 
     if (!course) return res.status(404).send('The course with the given ID was not found.');
