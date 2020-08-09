de376f799659b10a1dbd131e988bdc50017a0983
Kiet Nguyen <kanguyen.vn@gmail.com>
2020-08-08 23:50:35 -0500

    Merged

--- backend/startup/prod.js
+++ backend/startup/prod.js
@@ -4,14 +4,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 module.exports = function(app) {
-<<<<<<< HEAD
-	app.use(helmet()); // encrypt the jwt
-    app.use(compression());
-    app.use(morgan('dev')); // log HTTP requests
-=======
 	app.use(helmet());
     app.use(compression());
     app.use(morgan('dev'));
->>>>>>> 4579e3c266e1e4d962386b99d1f6250d363104bf
     app.use(cors())
 }
