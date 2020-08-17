87d3396fbe9d20d0e6ca9e0f0519df608908eb36
hikarimn <hikari.r.mine@gmail.com>
2020-08-17 00:09:57 -0400

    wip

--- frontend/src/components/SignUpDialog.jsx
+++ frontend/src/components/SignUpDialog.jsx
@@ -7,8 +7,6 @@ import {
   DialogContent,
   DialogTitle,
   DialogContentText,
-  FilledInput,
-  FormControl,
   Input,
   InputLabel,
   InputAdornment,
@@ -16,15 +14,11 @@ import {
   Select,
   MenuItem,
   ListItemText,
-  Checkbox,
-  makeStyles,
-  useTheme
+  Checkbox
 } from "@material-ui/core";
-import { Visibility, VisibilityOff, SignalCellularConnectedNoInternet0Bar } from "@material-ui/icons";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
 
 const SignUpDialog = ({ onClose, openState }) => {
-
-  // password stuff
   const [values, setValues] = React.useState({
     password: "",
     showPassword: false,
@@ -42,71 +36,19 @@ const SignUpDialog = ({ onClose, openState }) => {
     event.preventDefault();
   };
 
-  // majors stuff
-
   const majors = [
-    "Computer Science",
-    "English",
-    "Physics",
-    "Economics",
-    "Music"
-  ]
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+  ];
 
-  const useStyles = makeStyles((theme) => ({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-      maxWidth: 300,
-    },
-    chips: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    chip: {
-      margin: 2,
-    },
-    noLabel: {
-      marginTop: theme.spacing(3),
-    },
-  }));
-
-  const classes = useStyles();
-  const theme = useTheme();
-  const [personName, setPersonName] = React.useState([]);
-
-  const ITEM_HEIGHT = 48;
-  const ITEM_PADDING_TOP = 8;
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 250,
-      },
-    },
-  };
-
-  function getStyles(name, personName, theme) {
-    return {
-      fontWeight:
-        personName.indexOf(name) === -1
-          ? theme.typography.fontWeightRegular
-          : theme.typography.fontWeightMedium,
-    };
-  }
-
-  const handleChangeMultiple = (event) => {
-    const { options } = event.target;
-    const value = [];
-    for (let i = 0, l = options.length; i < l; i += 1) {
-      if (options[i].selected) {
-        value.push(options[i].value);
-      }
-    }
-    setPersonName(value);
-  };
-
-
-  // class year stuff
   const [selectedDate, handleDateChange] = useState(new Date().getFullYear());
 
   return (
@@ -117,11 +59,9 @@ const SignUpDialog = ({ onClose, openState }) => {
     >
       <DialogTitle id="form-dialog-title">Sign Up</DialogTitle>
       <DialogContent>
-
         <DialogContentText>
           You have to have a Lawrence email to sign up for an account.
         </DialogContentText>
-
         <TextField
           autoFocus
           //margin="dense"
@@ -130,11 +70,9 @@ const SignUpDialog = ({ onClose, openState }) => {
           type="email"
           fullWidth
         />
-
         <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
         <Input
           id="standard-adornment-password"
-          label="Password"
           type={values.showPassword ? "text" : "password"}
           value={values.password}
           onChange={handleChange("password")}
@@ -151,59 +89,30 @@ const SignUpDialog = ({ onClose, openState }) => {
           }
           fullWidth
         />
-
         <TextField
           autoFocus
-          margin="dense"
+          //margin="dense"
           id="name"
           label="Name"
           type="string"
           fullWidth
         />
-
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-mutiple-name-label">Major(s)</InputLabel>
-          <Select
-            labelId="demo-mutiple-name-label"
-            id="demo-mutiple-name"
-            multiple
-            value={personName}
-            onChange={handleChange}
-            input={<Input />}
-            MenuProps={MenuProps}
-            fullWidth
-          >
-            {majors.map((name) => (
-              <MenuItem key={name} value={name} style={getStyles(name, personName, theme)}>
-                {name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-        <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={personName}
-          onChange={handleChange}
-          multiple
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-
-        {/* <TextField
+        <TextField
           autoFocus
-          margin="dense"
+          //margin="dense"
+          id="major"
+          label="Major"
+          type="string"
+          fullWidth
+        />
+        <TextField
+          autoFocus
+          //margin="dense"
           id="classOf"
           label="Class of"
           type="number"
           fullWidth
-        /> */}
+        />
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} color="primary">
