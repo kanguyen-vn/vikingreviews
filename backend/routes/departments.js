5e71e4c573602b2ffb5c394a5a46782beae001f1
Kiet Nguyen <kanguyen.vn@gmail.com>
2020-08-20 11:15:48 -0500

    Update Department model, added 4-letter code

--- backend/routes/departments.js
+++ backend/routes/departments.js
@@ -1,47 +1,71 @@
-const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
-const validateObjectId = require('../middleware/validateObjectId');
-const { Department, validateDepartment } = require('../models/department');
-const validate = require('../middleware/validate');
-const express = require('express');
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const validateObjectId = require("../middleware/validateObjectId");
+const { Department, validateDepartment } = require("../models/department");
+const validate = require("../middleware/validate");
+const express = require("express");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const departments = await Department.find().sort('name');
-    res.send(departments);
+router.get("/", async (req, res) => {
+  const departments = await Department.find().sort("code");
+  res.send(departments);
 });
 
-router.post('/', [auth, admin, validate(validateDepartment)], async (req, res) => {
-    let department = new Department({ name: req.body.name });
+router.post(
+  "/",
+  [auth, admin, validate(validateDepartment)],
+  async (req, res) => {
+    let department = new Department({
+      code: req.body.code,
+      name: req.body.name,
+    });
     department = await department.save();
 
     res.send(department);
-});
+  }
+);
 
-router.put('/:id', [auth, admin, validateObjectId, validate(validateDepartment)], async (req, res) => {
-    const department = await Department.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
-        new: true
-    });
+router.put(
+  "/:id",
+  [auth, admin, validateObjectId, validate(validateDepartment)],
+  async (req, res) => {
+    const department = await Department.findByIdAndUpdate(
+      req.params.id,
+      { code: req.body.code, name: req.body.name },
+      {
+        new: true,
+      }
+    );
 
-    if (!department) return res.status(404).send('The department with the given ID was not found.');
-
-    res.send(department);
-});
-
-router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
-    const department = await Department.findByIdAndRemove(req.params.id);
-
-    if (!department) return res.status(404).send('The department with the given ID was not found.');
-
-    res.send(department);
-});
-
-router.get('/:id', validateObjectId, async (req, res) => {
-    const department = await Department.findById(req.params.id);
-
-    if (!department) return res.status(404).send('The department with the given ID was not found.');
+    if (!department)
+      return res
+        .status(404)
+        .send("The department with the given ID was not found.");
 
     res.send(department);
+  }
+);
+
+router.delete("/:id", [auth, admin, validateObjectId], async (req, res) => {
+  const department = await Department.findByIdAndRemove(req.params.id);
+
+  if (!department)
+    return res
+      .status(404)
+      .send("The department with the given ID was not found.");
+
+  res.send(department);
+});
+
+router.get("/:id", validateObjectId, async (req, res) => {
+  const department = await Department.findById(req.params.id);
+
+  if (!department)
+    return res
+      .status(404)
+      .send("The department with the given ID was not found.");
+
+  res.send(department);
 });
 
 module.exports = router;
