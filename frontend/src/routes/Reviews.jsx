9aa6e456b516a8599595ca48490dd7850f36fc2e
hikarimn <hikari.r.mine@gmail.com>
2020-08-23 17:06:41 -0400

    change on width

--- frontend/src/routes/Reviews.jsx
+++ frontend/src/routes/Reviews.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import useWindowDimensions from '../misc/useWindowDimensions';
-import clsx from 'clsx';
-import { lighten, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import Grid from '@material-ui/core/Grid';
-
-
+import {
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  Grid,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableSortLabel
+} from '@material-ui/core';
 
 function createData(department_code, course_num, class_name, review_sum, overall, workload, enthusiasm, textbook) {
   return {
@@ -95,8 +83,7 @@ const headCells = [
 ];
 
 function EnhancedTableHead(props) {
-  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
-  const { height, width } = useWindowDimensions();
+  const { order, orderBy, onRequestSort } = props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
   };
@@ -118,11 +105,6 @@ function EnhancedTableHead(props) {
               onClick={createSortHandler(headCell.id)}
             >
               {headCell.label}
-              {/* {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null} */}
             </TableSortLabel>
           </TableCell>
         ))}
@@ -175,10 +157,10 @@ const useStyles = makeStyles((theme) => ({
       font: '9px',
     },
     [theme.breakpoints.between('sm', 'md')]: {
-      width: '55vh',
+      width: '54vh',
     },
     [theme.breakpoints.between('md', 'lg')]: {
-      width: '80vh',
+      width: '84vh',
     },
     [theme.breakpoints.between('lg', 'xl')]: {
       width: '115vh',
@@ -257,7 +239,7 @@ export default function EnhancedTable() {
                         tabIndex={-1}
                         key={row.name}
                       >
-                        
+
                         <TableCell align='center' component="th" id={labelId} scope="row" padding="none">
                           {row.department_code}
                         </TableCell>
