c922b2998f1e2467d40eba86b220ef95bc44c09c
hikarimn <hikari.r.mine@gmail.com>
2020-09-03 17:33:37 -0400

    wip

--- frontend/src/routes/Course.jsx
+++ frontend/src/routes/Course.jsx
@@ -14,6 +14,8 @@ import ScrollableGrid from "../components/common/ScrollableGrid";
 import AddReview from "../components/AddReview";
 import courses from "../services/courseService";
 import { Typography } from "@material-ui/core";
+import reviews from "../services/reviewService";
+import auth from "../services/authService";
 
 const useStyles = (theme) => ({
   pageStyles: {
@@ -23,11 +25,43 @@ const useStyles = (theme) => ({
 });
 
 class Course extends Component {
+  state = {reviews: [] };
+  async componentDidMount(){
+    const courseId = this.props.match.params.id;
+    const data = await reviews.getById(courseId);
+    this.setState({ reviews: data });
+  }
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     user: null,
+  //     reviews: []
+  //   };
+  //   this.getCourseById = this.getCourseById.bind(this);
+  //   console.log('*************************************************');
+  //   console.log(props);
+  //   console.log('*************************************************');
+  // };
+  
+  // async getCourseById(){
+  //   const courseId = this.props.match.params.id;
+  //   const Reviews = await reviews.getById(courseId);
+  //   console.log(Reviews);
+  //   this.setState({Reviews});
+  // }
+
+  // async componentDidMount() {
+  //   const user = auth.currentUser();
+  //   this.setState({ user });
+  //   await this.getCourseById(true);
+  // }
+
   render() {
     const { classes, user, draw, ...other } = this.props;
     const detail = other.location.state
       ? other.location.state.detail
       : courses.getById(other.match.params.id);
+    console.log(detail);
     return (
       (!user && <LoginError draw={draw} {...other} />) || (
         <Grid
@@ -36,7 +70,15 @@ class Course extends Component {
           justify="center"
           alignItems="center"
         >
-          <Typography>{other.match.params.id}</Typography>
+          <Grid>
+            <Typography>
+              {detail.left}
+            </Typography>
+            <Typography>
+              {detail.right}
+            </Typography>
+          </Grid>
+          {/* <Typography>{other.match.params.id}</Typography> */}
           <StyledButton
             onClick={() => draw({ detail, ...other })(AddReview)}
             text="Add Review"
---
+++ frontend/src/services/reviewService.js
@@ -0,0 +1,17 @@
+import http from "./httpService";
+import { uri } from "../config.json";
+
+const apiEndpoint = uri + "/api/reviews";
+
+async function getById(id) {
+    // if (localStorage.departments) {
+    //   return JSON.parse(localStorage.departments).find(
+    //     (department) => department._id === id
+    //   );
+    // }
+    // http://localhost:3000/api/reviews?course=5f467c93d475cd0578930bde
+    const { data } = await http.get(`${apiEndpoint}?course=${id}`);
+    return data;
+  }
+  
+  export default { getById };
